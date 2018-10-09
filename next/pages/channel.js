bc667ec264f7400af02be5a9fe544eb76afa238e
Renato Ribeiro <renatoribroman@gmail.com>
2018-10-09 03:38:40 -0300

    feat(channel): add 2000ms pollInterval to channel messages

--- next/pages/channel.js
+++ next/pages/channel.js
@@ -10,7 +10,6 @@ import Link from 'next/link'
 import { HashLoader } from 'react-spinners'
 import App from 'grommet/components/App'
 import ChatIcon from 'grommet/components/icons/base/Chat'
-import RefreshIcon from 'grommet/components/icons/base/Refresh'
 import AddCircleIcon from 'grommet/components/icons/base/Add'
 import UserIcon from 'grommet/components/icons/base/User'
 import LogoutIcon from 'grommet/components/icons/base/Logout'
@@ -115,8 +114,6 @@ const ChatRoom = ({ url, url: { query: { channel = 'general' } } }) => (
                           <Title>
                             { '#' + channel }
                           </Title>
-
-                          <Button icon={ <RefreshIcon /> } onClick={ () => refetch() } />
                         </StyledRoomHeader>
 
                         <Box pad='medium' flex='grow'>
