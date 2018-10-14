fc21aff771769159c8f3061dabf5d4d647afa405
Renato Ribeiro <renatoribroman@gmail.com>
2018-10-14 00:05:08 -0300

    feat(mention): comment regex const

--- next/src/modules/mention/lib/const.js
+++ next/src/modules/mention/lib/const.js
@@ -1,3 +1,10 @@
+// pattern used in react-mentions
 export const MENTION_PATTERN = '@[__display__](__id__)'
+
+// capture full mention in a single group
+// eg.: @[renatorib](2) => ['@[renatorib](2)']
 export const MENTION_MATCH = /(@\[\w+\]\(\d+\))/i
+
+// capture mention in two groups: name and id
+// eg.: @[renatorib](2) => ['renatorib', 2]
 export const MENTION_GROUPS = /@\[(\w+)\]\((\d+)\)/i
