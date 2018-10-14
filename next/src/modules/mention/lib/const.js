28a37f84262119fa62f09f2c441be7a79f5671da
Renato Ribeiro <renatoribroman@gmail.com>
2018-10-14 00:08:20 -0300

    fix(mention): allow numbers in user mention regex

--- next/src/modules/mention/lib/const.js
+++ next/src/modules/mention/lib/const.js
@@ -3,8 +3,8 @@ export const MENTION_PATTERN = '@[__display__](__id__)'
 
 // capture full mention in a single group
 // eg.: @[renatorib](2) => ['@[renatorib](2)']
-export const MENTION_MATCH = /(@\[\w+\]\(\d+\))/i
+export const MENTION_MATCH = /(@\[.+\]\(\d+\))/i
 
 // capture mention in two groups: name and id
 // eg.: @[renatorib](2) => ['renatorib', 2]
-export const MENTION_GROUPS = /@\[(\w+)\]\((\d+)\)/i
+export const MENTION_GROUPS = /@\[(.+)\]\((\d+)\)/i
